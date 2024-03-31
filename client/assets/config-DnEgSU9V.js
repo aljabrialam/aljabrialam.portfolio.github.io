f29d6a63b65c3a49b37a5699c6fad13b0dce2e97
aljabrialam <aljabrialam@users.noreply.github.com>
2024-03-31 07:46:52 +0000

    deploy: 11bd4471919a5e7bccd3cc0edb72616e1bccd580

---
+++ client/assets/config-DnEgSU9V.js
@@ -0,0 +1,5 @@
+const i="ALJABRI ALAM",t="SOFTWARE ENGINEER",n=["Adventurous","Creative","Pragmatic","Family-oriented","Optimistic"],s="",c="",o="",e="aljabrialam",a="",r=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,l={name:i,role:t,disciplines:n,url:s,twitter:c,figma:o,github:e,repo:a,ascii:r};export{l as c};
